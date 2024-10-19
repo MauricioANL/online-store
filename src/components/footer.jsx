48f2d23dded7dcf169bcf231fa6a5780aaed2aed
MauricioANL <Mauricio.Navarro@sdgku.edu>
2024-10-18 23:18:56 -0700

    Assignment 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import './styles/footer.css';
+
+function Footer(){
+    return(
+        <div className="footer">
+            <hr></hr>
+            <footer className="content">
+                <div>
+                    <span>Created by: Mauricio Navarro</span>
+                </div>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer;
